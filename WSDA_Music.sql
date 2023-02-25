be62358c3962fecbfc30bfad4dfba30357140fa1
haannah <karetihannah@gmail.com>
2023-02-25 12:08:35 -0800

    Update WSDA_Music.sql

--- WSDA_Music.sql
+++ WSDA_Music.sql
@@ -220,3 +220,51 @@ ORDER BY
 		BillingCity
 		
 		
+/* 
+Description: JOINS
+*/
+
+SELECT
+	c.LastName,
+	c.FirstName,
+	i.InvoiceId,
+	i.CustomerId,
+	i.InvoiceDate,
+	i.total
+FROM
+	Invoice AS i
+LEFT OUTER JOIN
+	Customer AS c
+ON 
+	i.CustomerId = c.CustomerId
+order by c.CustomerId
+
+/* DEscriptions: Joins on more than two tables | What employees are responsible for the 10 heighest indivuals sales 
+*/
+
+
+SELECT
+	e.FirstName,
+	e.LastName,
+	e.EmployeeId, 
+	c.FirstName,
+	c.LastName,
+	c.SupportRepId,
+	i.CustomerId,
+	i.total
+FROM
+	Invoice AS i 
+INNER JOIN
+	Customer AS c 
+ON 
+	i.CustomerId = c.CustomerId
+INNER JOIN 
+	Employee AS e 
+ON 
+	c.SupportRepId = e.EmployeeId
+ORDER BY 
+	i.total DESC
+LIMIT 10 
+
+
+
