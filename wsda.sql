9f3f292b614dde639a0b5e4b2f06194eca9745ec
haannah <karetihannah@gmail.com>
2023-02-27 11:49:53 -0800

    Update wsda.sql

--- wsda.sql
+++ wsda.sql
@@ -12,3 +12,19 @@ FROM
 	Customer
 WHERE
 	Country = 'USA'
+
+/*
+Description: How is each indiviual city performing againist the global average sales ?
+*/
+
+
+SELECT
+	BillingCity,
+	AVG(total) AS [City Average],
+	(SELECT avg(total) from Invoice) AS [Global Average]
+FROM
+	Invoice 
+GROUP BY 
+	BillingCity
+ORDER BY 
+	BillingCity
