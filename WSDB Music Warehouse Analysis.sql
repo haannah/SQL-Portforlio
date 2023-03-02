ff715134eb1b7e746e5ab9a9303a92af4ccd7205
haannah <karetihannah@gmail.com>
2023-03-02 12:33:47 -0800

    Update WSDB Music Warehouse Analysis.sql

--- WSDB Music Warehouse Analysis.sql
+++ WSDB Music Warehouse Analysis.sql
@@ -10,4 +10,30 @@ WHERE
 	
 /* How much money did WSDA Music make during the same period? */ 
 
-Select
+SELECT 
+	SUM(total)
+From 
+	Invoice
+WHERE
+	InvoiceDate >= "2011-01-01" AND InvoiceDate <= "2012-12-31"
+
+
+/*Get a list of customers who made purchases between 2011 and 2012.*/
+
+ SELECT
+	c.FirstName,
+	c.LastName,
+	i.total
+FROM
+	invoice i 
+INNER JOIN
+	Customer c 
+ON i.CustomerId = c.CustomerId
+WHERE
+	InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
+ORDER BY 
+	i.total DESC
+	
+	
+
+	
